1a12be53d24309b2ed88d3395080538792fcbff5
ditramadia <ditrarizqaamadia@gmail.com>
2024-02-19 18:40:41 +0700

    feat: add overflow menu

--- app/src/main/java/com/example/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/marsrealestate/overview/OverviewFragment.kt
@@ -3,6 +3,8 @@ package com.example.marsrealestate.overview
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.marsrealestate.R
@@ -16,6 +18,19 @@ class OverviewFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         val binding = FragmentOverviewBinding.inflate(inflater)
+
+        // Enable options
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    /**
+     * Inflates the overflow menu that contains filtering options.
+     */
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.overflow_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 }
